40af7a3ef774e48390c99f3aae514de0d488ee33
jusung lee <jusunglee1459@gmail.com>
2020-02-15 11:28:49 -0800

    Ragdoll

---
+++ Assets/RotatePlayer.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotatePlayer : MonoBehaviour
+{
+    private Rigidbody rb;
+    public float rotateSpeed;
+
+    Vector3 rotationLeft;
+    Vector3 rotationRight;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();    
+    }
+
+    void FixedUpdate()
+    {
+        rotationLeft.Set(0f, -rotateSpeed, 0f);
+        rotationRight.Set(0f, rotateSpeed, 0f);
+
+        rotationLeft = -rotationLeft.normalized * -rotateSpeed;
+        rotationRight = rotationRight.normalized * rotateSpeed;
+
+        Quaternion deltaRotationLeft = Quaternion.Euler(rotationLeft * Time.fixedDeltaTime);
+        Quaternion deltaRotationRight = Quaternion.Euler(rotationRight * Time.fixedDeltaTime);
+
+        if(Input.GetKey(KeyCode.Q))
+        {
+            rb.MoveRotation(rb.rotation * deltaRotationLeft);
+        }
+
+        if(Input.GetKey(KeyCode.E))
+        {
+            rb.MoveRotation(rb.rotation * deltaRotationRight);
+        }
+    }
+
+}
