614e06a93b4777f3d210682bc9379343e91f52cb
Anya <ania.leonova@gmail.com>
2020-02-15 12:45:29 -0800

    Added npc script

---
+++ Assets/NpcScript.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NpcScript : MonoBehaviour
+{
+    public float speed;
+
+    private Rigidbody body;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        body = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 movement = new Vector3(10.0f, 0.0f, 0.0f);
+
+        body.AddForce(movement * speed);
+    }
+}
