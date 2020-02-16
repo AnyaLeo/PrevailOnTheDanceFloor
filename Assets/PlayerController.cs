c54582236d1cbc8fbf0048a07e12b00104d189e5
Anya <ania.leonova@gmail.com>
2020-02-15 16:41:41 -0800

    Changes to Player ragdoll character - hips have the spring attached to Com player now

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -8,9 +8,16 @@ public class PlayerController : MonoBehaviour
 
     private Rigidbody rb;
 
+    // Get the force that is holding the player upright
+    ConstantForce objectForce;
+
     private void Start()
     {
         rb = GetComponent<Rigidbody>();
+
+        SpringJoint joint = GetComponent<SpringJoint>();
+        Rigidbody connectedComponent = joint.connectedBody;
+        objectForce = connectedComponent.GetComponent<ConstantForce>();
     }
 
     private void FixedUpdate()
@@ -20,6 +27,21 @@ public class PlayerController : MonoBehaviour
 
         Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
 
+        // Calculate the appropriate y value for the upward force
+        float yForce = Mathf.Lerp(0.0f, 170.0f, 170.0f - objectForce.force.x);
+
+        Vector3 tilt = new Vector3(0.0f, yForce, 0.0f);
+        Vector3 previousForce =
+            new Vector3(objectForce.force.x * moveHorizontal * 0.1f,
+                        0.0f,
+                        objectForce.force.z * moveVertical * 0.1f);
+
+        objectForce.force = tilt + previousForce;
+
+        // Change the tilt of the player depending on which way they are going
+        //objectForce.force = tilt + movement;
+
+        // Move the player in that direction
         rb.AddForce(movement * speed);
     }
 }
