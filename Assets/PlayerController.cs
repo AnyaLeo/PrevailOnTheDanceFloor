3fd4d82d208d43732eec564dd3400e3ae7ede086
Anya <ania.leonova@gmail.com>
2020-02-15 17:28:31 -0800

    Walking now makes the player more tilted

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -8,13 +8,13 @@ public class PlayerController : MonoBehaviour
 
     private Rigidbody rb;
 
-    // Get the force that is holding the player upright
     ConstantForce objectForce;
 
     private void Start()
     {
         rb = GetComponent<Rigidbody>();
 
+        // Get the force that is holding the player upright
         SpringJoint joint = GetComponent<SpringJoint>();
         Rigidbody connectedComponent = joint.connectedBody;
         objectForce = connectedComponent.GetComponent<ConstantForce>();
@@ -28,18 +28,16 @@ public class PlayerController : MonoBehaviour
         Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
 
         // Calculate the appropriate y value for the upward force
-        float yForce = Mathf.Lerp(0.0f, 170.0f, 170.0f - objectForce.force.x);
+        float yForceValueToClamp = 170.0f - (Mathf.Abs(objectForce.force.x) + Mathf.Abs(objectForce.force.z));
+        float yForce = Mathf.Clamp(yForceValueToClamp, 0.0f, 170.0f);
 
+        // Apply the object force corresponding to which direction the character is walking in
         Vector3 tilt = new Vector3(0.0f, yForce, 0.0f);
         Vector3 previousForce =
-            new Vector3(objectForce.force.x * moveHorizontal * 0.1f,
+            new Vector3(objectForce.force.x + moveHorizontal * 0.1f,
                         0.0f,
-                        objectForce.force.z * moveVertical * 0.1f);
-
+                        objectForce.force.z + moveVertical * 0.1f);
         objectForce.force = tilt + previousForce;
-
-        // Change the tilt of the player depending on which way they are going
-        //objectForce.force = tilt + movement;
 
         // Move the player in that direction
         rb.AddForce(movement * speed);
