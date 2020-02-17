5699a22c0a4a6d860028abaf09818968e508e941
Juan Lee <yosohs@gmail.com>
2020-02-16 16:21:58 -0800

    Color Changes

---
+++ Assets/ChangeColors.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeColors : MonoBehaviour
+{
+    private float latestDirectionChangeTime;
+    public float directionChangeTime = 3f;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Color newColor = new Color(Random.value, Random.value, Random.value, 1.0f);
+        GetComponent<Renderer>().material.color = newColor;
+
+        latestDirectionChangeTime = 0f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Color newColor = new Color(Random.value, Random.value, Random.value, 1.0f);
+
+        if (Time.time - latestDirectionChangeTime > directionChangeTime)
+        {
+            newColor = new Color(Random.value, Random.value, Random.value, 1.0f);
+            GetComponent<Renderer>().material.color = newColor;
+            latestDirectionChangeTime = Time.time;
+        }
+    }
+}
